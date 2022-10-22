5feb36c1557d6a09629e3f6d9c033b2d5696845e
showkubii <52117480+showkubii@users.noreply.github.com>
2022-10-22 10:41:55 +0700

    Update gclone.go

--- gclone.go
+++ gclone.go
@@ -4,9 +4,9 @@
 package main
 
 import (
-	_ "github.com/showkubii/gclone-diynez-happy/backend/all" // import all backends
+	_ "github.com/gclone-diynez-happy/gclone/backend/all" // import all backends
 	"github.com/rclone/rclone/cmd"
-	_ "github.com/showkubii/gclone-diynez-happy/cmd/all"    // import all commands
+	_ "github.com/gclone-diynez-happy/gclone/cmd/all"    // import all commands
 	_ "github.com/rclone/rclone/lib/plugin" // import plugins
 )
 
